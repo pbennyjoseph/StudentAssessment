1ad45a046451339c26993d621eb1a3840bfa69c2
Benny Joseph <jbenny37@gmail.com>
2019-11-09 19:54:59 +0530

    added InstructorPanel and webClient

---
+++ src/main/java/InstructorPanel.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class InstructorPanel extends JPanel implements ActionListener {
+    InstructorPanel() {
+        setLayout(new BorderLayout());
+        JLabel welcome = new JLabel("Welcome Instructor!", JLabel.CENTER);
+        JPanel content = new JPanel(new FlowLayout());
+        JButton addQuiz = new JButton("Add Quiz");
+        JButton evaluateMarks = new JButton("Evaluate Responses");
+        evaluateMarks.setFocusable(false);
+        evaluateMarks.setActionCommand("eval");
+        evaluateMarks.addActionListener(this);
+        addQuiz.setFocusable(false);
+        addQuiz.addActionListener(this);
+
+        content.add(addQuiz);
+        content.add(evaluateMarks);
+
+        add(welcome, BorderLayout.NORTH);
+        add(content, BorderLayout.CENTER);
+
+        setVisible(true);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("eval")) {
+
+        } else {
+
+        }
+
+    }
+
+//    public static void main(String[] args) {
+//        SwingUtilities.invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
+//                JFrame x = new JFrame();
+//                x.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+//                x.setLayout(new BorderLayout());
+//                x.add(new InstructorPanel());
+//                x.pack();
+//                x.setVisible(true);
+//            }
+//        });
+//    }
+}
--- src/main/java/StudentAssessment.java
+++ src/main/java/StudentAssessment.java
@@ -2,18 +2,34 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class StudentAssessment extends JFrame implements ActionListener {
-    private JPanel mainPanel, throbber;//, login, requestNewPassword, questions, uploadAnswer;
+    private static final String baseURL = "http://localhost/javamini/index.php?";
+    private JPanel loginPanel, throbber, mainCardPanel;//, login, requestNewPassword, questions, uploadAnswer;
+    private JPasswordField pwd;
+    private JButton logoutButton;
+    private JTextField username;
+    private JLabel thr;
+    private CardLayout cx;
+    private webClient wx;
 
     private void createLoader() {
-        throbber = new JPanel(new FlowLayout());
-        throbber.setVisible(false);
-        java.net.URL throbberURL = getClass().getResource("./ajax-loader.gif");
+        throbber = new JPanel(new BorderLayout());
+//        throbber.setVisible(false);
+        java.net.URL throbberURL = getClass().getResource("./def-loader.gif");
         ImageIcon loader = new ImageIcon(throbberURL);
-        JLabel thr = new JLabel("Logging you in...", loader, JLabel.CENTER);
-        thr.setVisible(true);
-        throbber.add(thr);
+        thr = new JLabel("Logging you in...", loader, JLabel.CENTER);
+        thr.setVisible(false);
+        logoutButton = new JButton("Logout");
+        logoutButton.setVisible(false);
+        logoutButton.setFocusable(false);
+        logoutButton.setActionCommand("logout");
+        logoutButton.addActionListener(this);
+        throbber.add(logoutButton, BorderLayout.EAST);
+        throbber.add(thr, BorderLayout.CENTER);
+
     }
 
     private JPanel createMainPanel() {
@@ -21,23 +37,31 @@ public class StudentAssessment extends JFrame implements ActionListener {
         JPanel titlePanel = new JPanel(new FlowLayout());
         JLabel title = new JLabel("Login to Access Questions");
         titlePanel.add(title);
+
         m.setLayout(new GridLayout(4, 1));
+
         JPanel userPanel = new JPanel(new FlowLayout());
         userPanel.add(new JLabel("Username: "));
-        userPanel.add(new JTextField(20));
+        userPanel.add(username = new JTextField(20));
+
         JPanel pwdPanel = new JPanel(new FlowLayout());
         pwdPanel.add(new JLabel("Password: "));
-        pwdPanel.add(new JPasswordField(20));
+        pwdPanel.add(pwd = new JPasswordField(20));
+
         JPanel submitPanel = new JPanel(new FlowLayout());
         JButton submitLogin = new JButton("Login");
         submitLogin.setFocusable(false);
         submitLogin.setActionCommand("userLogin");
         submitLogin.addActionListener(this);
+
         submitPanel.add(submitLogin);
+
         m.add(titlePanel);
         m.add(userPanel);
+
         m.add(pwdPanel);
         m.add(submitPanel);
+
         return m;
     }
 
@@ -46,22 +70,44 @@ public class StudentAssessment extends JFrame implements ActionListener {
 //        setExtendedState(JFrame.MAXIMIZED_BOTH);
 
         setTitle("Login");
+        wx = new webClient();
 
-        mainPanel = createMainPanel();
+        loginPanel = createMainPanel();
         createLoader();
+        cx = new CardLayout();
+        mainCardPanel = new JPanel(cx);
         setLayout(new BorderLayout());
 
-        add(mainPanel, BorderLayout.CENTER);
+        mainCardPanel.add(loginPanel, "mainCard");
+
         add(throbber, BorderLayout.NORTH);
+//        mainCardPanel.add(throbber, "loaderCard");
+        JPanel ip = new InstructorPanel();
+        mainCardPanel.add(ip, "adminCard");
+
+        add(mainCardPanel, BorderLayout.CENTER);
         pack();
+
 
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent event) {
+                exitProcedure();
+            }
+        });
+
         setVisible(true);
     }
 
+    private void exitProcedure() {
+//        timerThread.setRunning(false);
+        System.exit(0);
+    }
+
     public static void main(String[] args) {
-//        new StudentAssessment();
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -73,31 +119,59 @@ public class StudentAssessment extends JFrame implements ActionListener {
     /**
      * Invoked when an action occurs.
      *
-     * @param e
+     * @param e The ActionEvent
      */
     @Override
     public void actionPerformed(ActionEvent e) {
-        try {
-            throbber.setVisible(true);
-            mainPanel.setVisible(false);
-            repaint();
+        if (e.getActionCommand().equals("userLogin")) {
+            thr.setVisible(true);
+            mainCardPanel.setVisible(false);
             SwingWorker<Boolean, Void> swingWorker = new SwingWorker<Boolean, Void>() {
+                int responseStatus;
+
                 @Override
                 protected Boolean doInBackground() throws Exception {
-                    Thread.sleep(2000);
+                    String x = wx.sendGet(baseURL + "user=" + username.getText()
+                            + "&pwd=" + new String(pwd.getPassword()));
+                    responseStatus = Integer.parseInt(x);
                     return true;
                 }
 
                 protected void done() {
-                    mainPanel.setVisible(true);
-                    throbber.setVisible(false);
+                    thr.setVisible(false);
+                    mainCardPanel.setVisible(true);
+                    if (responseStatus == 2) {
+                        username.setText("");
+                        pwd.setText("");
+                        JOptionPane.showMessageDialog(null, "You are now logged in as Student");
+                        CardLayout ctx = (CardLayout) mainCardPanel.getLayout();
+                        setExtendedState(MAXIMIZED_BOTH);
+                        logoutButton.setVisible(true);
+                        ctx.show(mainCardPanel, "adminCard");
+                    } else if (responseStatus == 1) {
+                        JOptionPane.showMessageDialog(null, "You are now logged in as Instructor");
+                        CardLayout ctx = (CardLayout) mainCardPanel.getLayout();
+                        setExtendedState(MAXIMIZED_BOTH);
+                        logoutButton.setVisible(true);
+                        ctx.show(mainCardPanel, "adminCard");
+                    } else {
+                        mainCardPanel.setVisible(true);
+                        JOptionPane.showMessageDialog(null, "Incorrect Details, try again");
+                    }
                     repaint();
                 }
-
             };
             swingWorker.execute();
-        } catch (Exception ex) {
-            System.out.println("Something happened");
+        } else if (e.getActionCommand().equals("logout")) {
+            logoutButton.setVisible(false);
+            StudentAssessment.main(null);
+            setVisible(false);
+            dispose();
+//            CardLayout ctx = (CardLayout) mainCardPanel.getLayout();
+//            ctx.previous(mainCardPanel);
+//            pack();
+//            Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+//            setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
         }
     }
 }
